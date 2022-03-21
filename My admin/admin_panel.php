1c4a536d7b12fbfd020a8eecaca597a2c197313c
Armstrong104 <62618934+Armstrong104@users.noreply.github.com>
2022-03-22 02:21:39 +0600

    Add files via upload

---
+++ My admin/admin_panel.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/adminstylesheet.css" rel="stylesheet">
+
+	<title>Admin Panel</title>
+	
+	
+  
+  </head>
+	
+  <body>
+    
+	<div id="header">
+		<center> 
+			<img src="images/admin_pic_50.png" alt="adminLogo" id="adminLogo">
+			<br> This is a admin panel
+		</center>
+	</div>
+	
+	<div id="sidebar">
+		<ul>
+			<li>Save DATA</li>
+			<li>Delete DATA</li>
+			<li>Update DATA</li>
+			<li>Developer</li>
+		</ul>
+	</div>
+	
+	<div id="data">
+		
+	</div>
+	
+	
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html>
